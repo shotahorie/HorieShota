98bd6f20d87a7e8ec484c83581589ad6ed4fd94f
driveline_14 <driveline_14@driveline14>
2024-04-24 18:35:23 +0900

    ああああ

---
+++ src/curriculum_B/Qes7.java
@@ -0,0 +1,45 @@
+package curriculum_B;
+
+import java.util.Scanner;
+
+public class Qes7 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+Scanner sc = new Scanner(System.in);
+int n = 0;
+System.out.println("生徒の人数を入力してください（2以上）：");
+n = sc.nextInt();
+
+//配列に教科入れて、
+//多次元配列に[点数][人数]
+
+
+
+
+
+
+
+
+
+
+
+int english = 0;
+int math = 0;
+int science = 0;
+int society = 0;
+int i = 0;
+for(i = 1;i <= n; i++) {
+	System.out.println(i + "人目の『英語』の点数を入力してください：");
+	english = sc.nextInt();
+	System.out.println(i + "人目の『数学』の点数を入力してください：");
+	math = sc.nextInt();
+	System.out.println(i + "人目の『英語』の点数を入力してください：");
+	 = sc.nextInt();
+	System.out.println(i + "人目の『英語』の点数を入力してください：");
+	english = sc.nextInt();
+
+}
+	}
+
+}
