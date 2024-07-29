b0b8184802fd884221bcaae092d8de4d5fbb3f1e
sakur <sakur@nbo>
2024-07-29 23:31:33 +0900

    1.27完成

--- 1.27/src/main/Main127.java
+++ 1.27/src/main/Main127.java
@@ -5,55 +5,57 @@ import java.util.Scanner;
 public class Main127 {
 
 	public static void main(String[] args) {
+		//scannerで文字列入力
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("コンソールに文字を入力してください");
+		String input = scanner.nextLine();
 
+		//文字列を「,」区切りで配列に格納
+		String[] animals = input.split(",");
 
-		Scanner sc = new Scanner(System.in);
-		sc.useDelimiter(",|¥n");
-		String str = sc.next();
-	       System.out.println("入力した文字列は " + str + " です。");
-//	        str = sc.next();
-	        System.out.println("入力した文字列は " + str + " です。");
+		//配列から変数に格納
+		for(String animal:animals) {
 
+			//文字列を「:」区切りで配列に格納
+			String[] data = animal.split(":");
+			String name = data[0];
+			String bodyLength = data[1];
+			String speed = data[2];
+			String animalName = "不明";
+
+			//switch文で学名に代入する
+			switch(name) {
+			case "ライオン":
+				animalName = "パンテラ レオ";
+				break;
+			case "ゾウ":
+				animalName = "ロキソドンタ・サイクロティス";
+				break;
+			case "パンダ":
+				animalName = "アイルロポダ・メラノレウカ";
+				break;
+			case "チンパンジー":
+				animalName = "パン・トゥログロディテス";
+				break;
+			case "シマウマ":
+				animalName = "チャップマンシマウマ";
+				break;
+			case "インコ":
+				animalName = "不明";
+				break;
+			}
+
+			//コンソールに出力する
+			System.out.println("動物名：" + name);
+			System.out.println("体長：" + bodyLength +"m");
+			System.out.println("速度：" + speed + "km/h");
+			System.out.println("学名：" + animalName);
+			System.out.println("");
+
+		}
 
 		//scannerを閉じる
-		sc.close();
+		scanner.close();
 	}
 
 }
-/*
-	コンソールに下記が入力されたときにコンソール出力結果がコンソールに表示されるように作ってください
-	ライオン:2.1:80,ゾウ:3.2:40,パンダ:1.9:30,チンパンジー:0.94:25,シマウマ:2.4:65,インコ:0.1:50
-	
-	コンソール出力結果
-	
-	コンソールに文字を入力してください
-	動物名：ライオン
-体長：2.1m
-速度：80km/h
-学名：パンテラ レオ
-
-動物名：ゾウ
-体長：3.2m
-速度：40km/h
-学名：ロキソドンタ・サイクロティス
-
-動物名：パンダ
-体長：1.9m
-速度：30km/h
-学名：アイルロポダ・メラノレウカ
-
-動物名：チンパンジー
-体長：0.94m
-速度：25km/h
-学名：パン・トゥログロディテス
-
-動物名：シマウマ
-体長：2.4m
-速度：65km/h
-学名：チャップマンシマウマ
-
-動物名：インコ
-体長：0.1m
-速度：50km/h
-学名：不明
-*/
